5c86445a24fae53bb28e784f62880e2304b3e1a2
Kirsten Nicholson <kirstennic6@gmail.com>
2021-02-05 19:25:48 +0000

    Piano tests passing

--- src/main/java/ShopStock/Piano.java
+++ src/main/java/ShopStock/Piano.java
@@ -1,4 +1,20 @@
 package ShopStock;
 
-public class Piano {
+public class Piano extends Instrument {
+
+    private String tuned;
+
+    public Piano(String itemType, double purchasePrice, double salePrice, String colour,
+                 String tuned) {
+        super(itemType, purchasePrice, salePrice, colour);
+        this.tuned = tuned;
+    }
+
+    public String isTuned() {
+        return "Piano sounds perfect!";
+    }
+
+    public String needsTuned(){
+        return "Piano sounds awful!";
+    }
 }
---
+++ src/test/java/PianoTest.java
@@ -0,0 +1,48 @@
+import ShopStock.Guitar;
+import ShopStock.Piano;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PianoTest {
+
+    Piano piano;
+
+    @Before
+
+    public void before(){
+        piano = new Piano("Kawai Digital Piano", 1000, 1200, "Satin Black", "Piano sounds great!");
+    }
+
+    @Test
+    public void hasItemType(){
+        assertEquals("Kawai Digital Piano", piano.getItemType());
+    }
+
+    @Test
+    public void hasPurchasePrice(){
+        assertEquals(1000, piano.getPurchasePrice(), 0.0);
+    }
+
+    @Test
+    public void hasSalePrice(){
+        assertEquals(1200, piano.getSalePrice() , 0.0);
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("Satin Black", piano.getColour());
+    }
+
+    @Test
+    public void pianoIsTuned(){
+        assertEquals("Piano sounds perfect!", piano.isTuned());
+    }
+
+    @Test
+    public void pianoNeedsTuned(){
+        assertEquals("Piano sounds awful!", piano.needsTuned());
+    }
+}
