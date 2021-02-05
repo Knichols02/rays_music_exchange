ceaba45b9300304131362c74be038c5eba4c78d3
Kirsten Nicholson <kirstennic6@gmail.com>
2021-02-05 19:33:02 +0000

    Trumpet tests passing

--- src/main/java/ShopStock/Trumpet.java
+++ src/main/java/ShopStock/Trumpet.java
@@ -1,4 +1,20 @@
 package ShopStock;
 
-public class Trumpet {
+public class Trumpet extends Instrument {
+
+    private int noOfValves;
+
+
+    public Trumpet(String itemType, double purchasePrice, double salePrice, String colour, int noOfValves) {
+        super(itemType, purchasePrice, salePrice, colour);
+        this.noOfValves = noOfValves;
+    }
+
+    public int getNoOfValves() {
+        return noOfValves;
+    }
+
+    public void setNoOfValves(int noOfValves) {
+        this.noOfValves = noOfValves;
+    }
 }
---
+++ src/test/java/TrumpetTest.java
@@ -0,0 +1,41 @@
+import ShopStock.Trumpet;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TrumpetTest {
+
+    Trumpet trumpet;
+
+    @Before
+
+    public void before(){
+        trumpet = new Trumpet("Swanky pTrumpet", 50, 100, "Purple", 4);
+    }
+
+    @Test
+    public void hasItemType(){
+        assertEquals("Swanky pTrumpet", trumpet.getItemType());
+    }
+
+    @Test
+    public void hasPurchasePrice(){
+        assertEquals(50, trumpet.getPurchasePrice(), 0.0);
+    }
+
+    @Test
+    public void hasSalePrice(){
+        assertEquals(100, trumpet.getSalePrice() , 0.0);
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("Purple", trumpet.getColour());
+    }
+
+    @Test
+    public void getNoOfValves(){
+        assertEquals(4, trumpet.getNoOfValves());
+    }
+}
