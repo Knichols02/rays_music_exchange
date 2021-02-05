ad114cb5418f404674254d0d10047faead97f811
Kirsten Nicholson <kirstennic6@gmail.com>
2021-02-05 20:01:23 +0000

    guitar string Tests passed

--- src/main/java/ShopStock/GuitarString.java
+++ src/main/java/ShopStock/GuitarString.java
@@ -1,4 +1,20 @@
 package ShopStock;
 
-public class GuitarString {
+public class GuitarString extends Accessories {
+
+    private String gauge;
+
+    public GuitarString(String itemType, double purchasePrice, double salePrice, int quantity,
+                        String gauge) {
+        super(itemType, purchasePrice, salePrice, quantity);
+        this.gauge = gauge;
+    }
+
+    public String getGauge() {
+        return gauge;
+    }
+
+    public void setGauge(String gauge) {
+        this.gauge = gauge;
+    }
 }
---
+++ src/test/java/GuitarStringTest.java
@@ -0,0 +1,40 @@
+import ShopStock.GuitarString;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GuitarStringTest {
+
+    GuitarString guitarString;
+
+    @Before
+    public void before(){
+        guitarString = new GuitarString("Ernie Ball", 6, 15, 30, "E" );
+    }
+
+    @Test
+    public void hasItemType(){
+        assertEquals("Ernie Ball", guitarString.getItemType());
+    }
+
+    @Test
+    public void hasPurchasePrice(){
+        assertEquals(6, guitarString.getPurchasePrice(), 0.0);
+    }
+
+    @Test
+    public void hasSalePrice(){
+        assertEquals(15, guitarString.getSalePrice() , 0.0);
+    }
+
+    @Test
+    public void hasQuantity(){
+        assertEquals(30, guitarString.getQuantity());
+    }
+
+    @Test
+    public void getGagueSize(){
+        assertEquals("E", guitarString.getGauge());
+    }
+}
